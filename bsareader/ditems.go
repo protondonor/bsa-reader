213c788a2985f496e86ec85c792c503c21ba7924
rowan <proton.donor.17@gmail.com>
2021-09-10 16:29:23 -0700

    parse ditems files

--- bsareader/ditems.go
+++ bsareader/ditems.go
@@ -27,3 +27,71 @@ type DungeonBlock struct {
 	IsStartingBlock bool
 	BlockIndex      uint8
 }
+
+func (d DItems) GetBlocks(name string) []string {
+	var blocks []DungeonBlock
+	for i := 0; i < len(d.Items); i++ {
+		if d.Items[i].LRE.Header.Name == name {
+			blocks = d.Items[i].Blocks
+		}
+	}
+
+	var result []string
+	for i := 0; i < len(blocks); i++ {
+		result = append(result, blocks[i].BlockInfo())
+	}
+
+	return result
+}
+
+func ReadDItems(bsa []byte) DItems {
+	count := udword(bsa[0:4])
+	var offsets []DungeonOffset
+	var items []DungeonInterior
+	for i := 0; i < int(count)*8; i += 8 {
+		offset := DungeonOffset{
+			Offset:       udword(bsa[i+4 : i+8]),
+			DungeonObjId: udword(bsa[i : i+4]),
+		}
+		offsets = append(offsets, offset)
+
+		lreOffset := offset.Offset + count*8 + 4
+
+		lre := ReadLocationRecordElem(bsa[lreOffset:])
+		dhOffset := lreOffset + uint32(lre.Len())
+		bc := word(bsa[dhOffset+3 : dhOffset+5])
+
+		var blocks []DungeonBlock
+		bOffset := dhOffset + 10
+		for j := 0; j < int(bc); j += 1 {
+			thisBlock := bOffset + 4*uint32(j)
+			bn, isb, bi := parseBlockInfo(word(bsa[thisBlock+2 : thisBlock+4]))
+
+			block := DungeonBlock{
+				X:               bsa[thisBlock],
+				Z:               bsa[thisBlock+1],
+				BlockNumber:     bn,
+				IsStartingBlock: isb,
+				BlockIndex:      bi,
+			}
+
+			blocks = append(blocks, block)
+		}
+
+		item := DungeonInterior{
+			LRE:    lre,
+			Header: DungeonHeader{BlockCount: bc},
+			Blocks: blocks,
+		}
+		items = append(items, item)
+	}
+
+	return DItems{Offsets: offsets, Items: items}
+}
+
+func parseBlockInfo(bnsi uint16) (uint16, bool, uint8) {
+	bi := uint8((bnsi & 0xF800) >> 11)
+	isb := (bnsi & 0x0400) >> 10
+	bn := bnsi & 0x03FF
+	return bn, isb != 0, bi
+}
--- bsareader/ditems_test.go
+++ bsareader/ditems_test.go
@@ -2,8 +2,75 @@ package bsareader_test
 
 import (
 	. "github.com/onsi/ginkgo"
-	//. "github.com/onsi/gomega"
-	//"github.com/rowanjacobs/bsa-reader/bsareader"
+	"io/ioutil"
+
+	. "github.com/onsi/gomega"
+	"github.com/rowanjacobs/bsa-reader/bsareader"
 )
 
-var _ = Describe("dItems", func() {})
+var _ = Describe("dItems", func() {
+	var cybiades []byte
+	BeforeEach(func() {
+		var err error
+		cybiades, err = ioutil.ReadFile("fixtures/cybiades.ditem")
+		Expect(err).NotTo(HaveOccurred())
+	})
+
+	It("reads offsets", func() {
+		/*
+		 * Each MapDItem record begins with an UInt32 count (DungeonCount)
+		 * of DungeonInterior elements contained within the record.
+		 */
+		dItems := bsareader.ReadDItems(cybiades)
+
+		Expect(dItems.Offsets).To(HaveLen(1))
+		// I feel like these are in reverse order but idk
+		Expect(dItems.Offsets[0].Offset).To(BeEquivalentTo(0))
+		Expect(dItems.Offsets[0].DungeonObjId).To(BeEquivalentTo(1))
+	})
+
+	It("reads dungeon records", func() {
+		dItems := bsareader.ReadDItems(cybiades)
+
+		Expect(dItems.Items).To(HaveLen(1))
+		Expect(dItems.Items[0].LRE.Doors).To(HaveLen(8))
+		Expect(dItems.Items[0].Header.BlockCount).To(BeEquivalentTo(12))
+		Expect(dItems.Items[0].Blocks[0].BlockInfo()).To(Equal("B0000007.RDB"))
+		Expect(dItems.Items[0].Blocks[1].BlockInfo()).To(Equal("B0000008.RDB"))
+		Expect(dItems.Items[0].Blocks[2].BlockInfo()).To(Equal("B0000003.RDB"))
+		Expect(dItems.Items[0].Blocks[3].BlockInfo()).To(Equal("N0000042.RDB"))
+	})
+
+	Context("reading multiple records", func() {
+		var betony []byte
+		BeforeEach(func() {
+			var err error
+			betony, err = ioutil.ReadFile("fixtures/betony.ditem")
+			Expect(err).NotTo(HaveOccurred())
+		})
+
+		It("reads dungeon records", func() {
+			dItems := bsareader.ReadDItems(betony)
+
+			Expect(dItems.Items).To(HaveLen(8))
+			Expect(dItems.Items[0].LRE.Header.Name).To(Equal("Tristore Laboratory"))
+			Expect(dItems.Items[0].Header.BlockCount).To(BeEquivalentTo(12))
+			Expect(dItems.Items[1].LRE.Header.Name).To(Equal("The Stronghold of Hearthhouse"))
+			Expect(dItems.Items[1].Header.BlockCount).To(BeEquivalentTo(10))
+			Expect(dItems.Items[2].LRE.Header.Name).To(Equal("The Cabal of Gwynona"))
+			Expect(dItems.Items[2].Header.BlockCount).To(BeEquivalentTo(11))
+			Expect(dItems.Items[3].LRE.Header.Name).To(Equal("Yeomcroft's Hold"))
+			Expect(dItems.Items[3].Header.BlockCount).To(BeEquivalentTo(8))
+		})
+
+		Context("GetBlocks", func() {
+			It("can retrieve blocks given a dungeon name", func() {
+				dItems := bsareader.ReadDItems(betony)
+
+				blocks := dItems.GetBlocks("The Yeomhouse Crypts")
+				Expect(blocks).To(HaveLen(5))
+				Expect(blocks).To(ConsistOf("B0000009.RDB", "B0000006.RDB", "M0000008.RDB", "B0000013.RDB", "B0000001.RDB"))
+			})
+		})
+	})
+})
